e52868b9ef3e29a89f6a4dc50feec8b9f5585152
Vahe Mikayelyan <vahemikayelyan@gmail.com>
2023-01-28 23:28:37 -0800

    added prod config

--- src/db/config/config.mjs
+++ src/db/config/config.mjs
@@ -15,11 +15,8 @@ const config = {
     dialect: "mysql",
   },
   production: {
-    database: env.DB_NAME,
-    username: env.DB_USERNAME,
-    password: env.DB_PASSWORD,
-    host: env.DB_HOST,
-    dialect: "mysql",
+    host: resolve("./", "database.sqlite"),
+    dialect: "sqlite",
   },
 };
 
